1baef3c0b3389e1c3d478cbf34e9f72f840cb7af
labdulla <laylaisa.211@gmail.com>
2023-11-13 19:41:16 +0300

    :)

--- Handlers/homeHandler.go
+++ Handlers/homeHandler.go
@@ -16,9 +16,15 @@ func init() {
 	))
 }
 func HomePageHandler(w http.ResponseWriter, r *http.Request) {
-	url := "https://groupietrackers.herokuapp.com/api/artists"
+	//https://mholt.github.io/json-to-go/
+	//urlArtists := "https://groupietrackers.herokuapp.com/api/artists"
+	urlLocations:= "https://groupietrackers.herokuapp.com/api/locations"
+	//urlDates:=	"https://groupietrackers.herokuapp.com/api/dates"
+	//urlRelation:=	"https://groupietrackers.herokuapp.com/api/relation"
 	artists := Artists{}
-	fetchData(url, &artists)
+	//fetchData(urlArtists, &artists)
+	fetch(urlLocations)
+
 	err := templates.ExecuteTemplate(w, "index.html", artists)
 	if err != nil {
 		http.Error(w, "Failed to render template", http.StatusInternalServerError)
--- Handlers/struct.go
+++ Handlers/struct.go
@@ -38,3 +38,20 @@ func fetchData(url string,data *Artists){
 		fmt.Printf("Data: %+v", data)
 	}
 }
+
+func fetch(url string){
+	response, err :=http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode ==http.StatusOK {
+		body, err :=ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(body))
+	}
+}
