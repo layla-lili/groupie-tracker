5678fd044ad95a810f4595996f4a33b1698affb0
Layla <laylaisa.211@gmail.com>
2024-01-08 00:41:50 +0300

    "init parse error"

--- Handlers/homeHandler.go
+++ Handlers/homeHandler.go
@@ -2,22 +2,31 @@ package Handlers
 
 import (
 	//"fmt"
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 )
 
 var templates *template.Template
 func init() {
-	// Load templates
-	templates = template.Must(
-		template.ParseFiles(
-		"templates/index.html",
-		"templates/404.html",
-		"templates/400.html",
-		"templates/405.html",
-		"templates/500.html",
-		"templates/details.html",
-	))
+// Load templates
+var err error
+templates, err = template.ParseFiles(
+	"templates/inde.html",
+	"templates/404.html",
+	"templates/400.html",
+	"templates/405.html",
+	"templates/500.html",
+	"templates/details.html",
+)
+if err != nil {
+	// Print the detailed error message
+	fmt.Println("Failed to parse templates:", err)
+	os.Exit(1)
+}
+
+
 }
 
 
